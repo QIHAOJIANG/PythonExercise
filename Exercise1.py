1193c308b88240f0dafaf6c892d79c228c86e34f
Qihao Jiang <qihao.jiang@epfl.ch>
2018-09-19 20:41:49 +0200

    the usage of input function of python3, the difference between python2, transfer string into integer

---
+++ Exercise1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is Exercise1 for september 09,2018.
+"""
+
+
+user1 = input("please input your name:")
+age1 = int(input("please input your age:"))
+
+print(type(user1))
+print(type(age1))
+
+print("100 years old:", 100-age1)
