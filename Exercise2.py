a95b928d0fc9dd5d4013aff2708df69d15b36b6d
Qihao Jiang <qihao.jiang@epfl.ch>
2018-09-19 21:14:49 +0200

    justify the odd/even for a number

---
+++ Exercise2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 19 20:47:45 2018, Exercise2
+
+@author: qihao
+"""
+
+number = int(input("input a number:"))
+if number%2 == 0:
+    print("the number is even")
+else:
+    print("the number is odd")
